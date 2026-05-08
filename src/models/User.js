41af52d95555363e06297f6f5a8f94eb0e071d88
Aayash <dangaayash@gmail.com>
2026-05-08 11:20:27 +0545

    src

---
+++ src/models/User.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase:true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        select: false
+    }
+}, { timestamps: true });
+
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+    this.password = await bcrypt.hash(this.password, 12);
+});
+
+userSchema.methods.comparePasswords = async function (candidatePassword) {
+    if( !candidatePassword || !this.password)return false;
+    return await bcrypt.compare(String(candidatePassword), this.password);
+}
+
+export default mongoose.model('User', userSchema);
